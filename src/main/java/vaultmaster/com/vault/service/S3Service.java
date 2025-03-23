2f7b03b6542e4c376b29760256a9a3191b0453bb
Julia Pallazola <pallazolaj@gmail.com>
2025-03-23 13:12:39 -0400

    File Upload

---
+++ src/main/java/vaultmaster/com/vault/service/S3Service.java
@@ -0,0 +1,59 @@
+package vaultmaster.com.vault.service;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.*;
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
+import org.springframework.stereotype.Service;
+import java.io.IOException;
+import java.util.UUID;
+
+@Service
+public class S3Service {
+
+    private final S3Client s3Client;
+    private final String bucketName = "vaultmaster-files";
+
+    public S3Service() {
+        DefaultCredentialsProvider credentialsProvider = DefaultCredentialsProvider.create();
+
+        try {
+            String accessKey = credentialsProvider.resolveCredentials().accessKeyId();
+            System.out.println("[DEBUG] AWS Access Key ID found: " + accessKey);
+        } catch (Exception e) {
+            System.err.println("[ERROR] Could not resolve AWS credentials: " + e.getMessage());
+        }
+
+        this.s3Client = S3Client.builder()
+                .region(Region.US_EAST_2)
+                .credentialsProvider(credentialsProvider)
+                .build();
+    }
+
+    public String uploadFile(byte[] fileBytes, String originalFilename) {
+        try {
+            String key = UUID.randomUUID() + "_" + originalFilename;
+
+            PutObjectRequest putRequest = PutObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(key)
+                    .build();
+
+            s3Client.putObject(putRequest, RequestBody.fromBytes(fileBytes));
+            return key;
+
+        } catch (S3Exception e) {
+            throw new RuntimeException("Failed to upload file to S3: " + e.awsErrorDetails().errorMessage(), e);
+        }
+    }
+
+    public byte[] downloadFile(String key) throws IOException {
+        GetObjectRequest getRequest = GetObjectRequest.builder()
+                .bucket(bucketName)
+                .key(key)
+                .build();
+
+        return s3Client.getObjectAsBytes(getRequest).asByteArray();
+    }
+}
